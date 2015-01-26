2fb333dc1d8455bdb8cf985c047b4a5ef67bfb63
Geert Kapteijns <geertkapteijns@wireless32-156.cwi.nl>
2015-01-26 14:19:04 +0100

    I can calculate the normalized benefit score for a given tree now.

--- quartet_method.rb
+++ quartet_method.rb
@@ -7,16 +7,25 @@ require "byebug"
 
 module Clustering
   class QuartetTree < Tree::TreeNode
+    COMPRESSION_LEVEL = 6
+
     # Construct tree with n leaf nodes, containing the n items, and
     # n - 2 internal nodes, labeled with a_{0} through a_{n-3}.
-    def self.random_tree(items)
+    # Accepts a hash with labels as keys and contents as values, as in:
+    #
+    # set = {
+    #   "Tangled Up In Blue" => "010100011111010...",
+    #   "You're a Big Girl Now" => "01010101010000...",
+    #   "If You See Her, Say Hello" => "01010100..."
+    # }
+    def self.random_tree(set)
       # Allows me to call leaves.next to add leaves in random order
-      leaves = items.shuffle.map { |item| QuartetTree.new(item) }.each
+      leaves = set.to_a.shuffle.map { |label, content| QuartetTree.new(label, content) }.each
       first_node = QuartetTree.new("n0")
       # First node gets two leaves.
       first_node.add(leaves.next); first_node.add(leaves.next)
 
-      random_tree = ( (1..items.size - 3).reduce(first_node) do |tree, i|
+      random_tree = ( (1..set.size - 3).reduce(first_node) do |tree, i|
         node = QuartetTree.new("n#{i}")
         node.add(leaves.next)
         tree << node
@@ -24,27 +33,45 @@ module Clustering
       end << leaves.next ).root
     end
 
-    def consistent_with?(first_pair, second_pair)
+    # Exactly one topology for a given group of four is consistent with any given tree.
+    # This method returns the consistent topology, e.g. [[1, 2], [3, 4]],
+    # meaning 12|34.
+    def consistent_topology(group_of_four)
+      topologies(group_of_four).detect { |topology| consistent_with?(topology) }
+    end
+
+    def consistent_with?(topology)
       # & is set intersection.
-      ( nodes_passed(*first_pair) & nodes_passed(*second_pair) ).empty?
+      ( nodes_passed(*topology.first) & nodes_passed(*topology.last) ).empty?
     end
 
     # (M - C{T}) / (M - m)
     def normalized_benefit_score
+      sum_of_maximal_costs = 0
+      sum_of_minimal_costs = 0
+      total_tree_cost = 0
+
+      groups_of_four.each do |group_of_four|
+        costs = costs_of_topologies(group_of_four)
+        sum_of_maximal_costs += costs.max
+        sum_of_minimal_costs += costs.min
+        
+        # I calculate the cost twice now. Not so nice.
+        total_tree_cost += cost( consistent_topology(group_of_four) )
+      end
+
+      (sum_of_maximal_costs - total_tree_cost) / (sum_of_maximal_costs - sum_of_minimal_costs)
     end
 
-    # C{T}
-    def total_cost
-
-    end
-
-    # TODO: something with content
-    def cost(first_pair, second_pair)
+    def cost(topology)
+      first_pair  = topology.first.map { |leaf| leaf.content }
+      second_pair = topology.last.map  { |leaf| leaf.content }
       normalized_compression_distance(*first_pair) + normalized_compression_distance(*second_pair)
     end
 
-    def quartet_topologies
-      each_leaf.map { |leaf| leaf.name }.combination(4) { |group_of_four| topologies(group_of_four) }
+    # Gives the three possible topologies for every combination of four labels
+    def groups_of_four
+      each_leaf.combination(4)
     end
 
     def topologies(set)
@@ -54,7 +81,12 @@ module Clustering
         [[ set[0], set[3] ], [ set[1], set[2] ]]
       ] 
     end
-    
+
+    # Gives the costs of the three different topologies for a group of four leaves.
+    def costs_of_topologies(group_of_four)
+      topologies(group_of_four).map { |topology| cost(topology) }
+    end
+
     private
 
     def normalized_compression_distance(a, b)
@@ -71,7 +103,7 @@ module Clustering
     end
 
     def parent_name(item)
-      find_by_name(item).parent.name
+      item.parent.name
     end
 
     # Gives the numbers of the internal nodes (n0, n1, ...) that must be
--- quartet_method_spec.rb
+++ quartet_method_spec.rb
@@ -1,21 +1,62 @@
+# A few remarks:
+#
+# The calculation of the normalized benefit score is untested.
+# The calculation of the normalized compression distance is untested.
+# I will have to think of a way of testing those things.
+
 require_relative "quartet_method"
 
 describe "QuartetTree" do
   before(:each) do
     @n0 = Clustering::QuartetTree.new("n0")
-    @n0 << Clustering::QuartetTree.new("item 1")
-    @n0 << Clustering::QuartetTree.new("item 2")
+    @leaf1 = Clustering::QuartetTree.new("item 1")
+    @leaf2 = Clustering::QuartetTree.new("item 2")
+    @leaf3 = Clustering::QuartetTree.new("item 3")
+    @leaf4 = Clustering::QuartetTree.new("item 4")
 
-    @kid = @n0 << Clustering::QuartetTree.new("n1") 
-    @kid << Clustering::QuartetTree.new("item 3")
-    @kid << Clustering::QuartetTree.new("item 4")
+    @n0 << @leaf1
+    @n0 << @leaf2
+
+    @n1 = @n0 << Clustering::QuartetTree.new("n1") 
+    @n1 << @leaf3
+    @n1 << @leaf4
   end
 
   it "knows that 12|34 is consistent" do
-    expect(@n0.consistent_with?(["item 1", "item 2"], ["item 3", "item 4"])).to eq true
+    expect(@n0.consistent_with?([[@leaf1, @leaf2], [@leaf3, @leaf4]])).to eq true
   end
   
   it "knows that 13|24 is not consistent" do
-    expect(@n0.consistent_with?(["item 1", "item 3"], ["item 2", "item 4"])).to eq false
+    expect(@n0.consistent_with?([[@leaf1, @leaf3], [@leaf2, @leaf4]])).to eq false
   end
+
+  it "selects 12|34| as the consistent topology" do
+    expect(@n0.consistent_topology([@leaf1, @leaf2, @leaf3, @leaf4])).to eq [[@leaf1, @leaf2], [@leaf3, @leaf4]]
+  end
+
+  it "always has exactly one consistent topology for every group of four items" do
+    10.times do
+      tree = Clustering::QuartetTree.random_tree(random_set) 
+      tree.groups_of_four.each do |group_of_four|
+        expect( tree.topologies(group_of_four).select { |topology| tree.consistent_with?(topology) }.size ).to eq 1
+      end
+    end
+  end
+
+  it "should calculate some kind of normalized benefit score" do
+    input = {
+      "Tangled Up In Blue" => "0010101010100000",
+      "You're A Big Girl Now" => "010101010101010",
+      "Buckets Of Rain" => "1110110101011101",
+      "Shelter From The Storm" => "010101011111010101"
+    }
+    tree = Clustering::QuartetTree.random_tree(input)
+    expect(tree.normalized_benefit_score).to be >= 0.0
+  end
+
+end
+
+def random_set
+  length = 4 + rand(6)
+  (0 .. length).map { |x| "item #{x}" }
 end
