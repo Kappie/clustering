c0a5f6303a61132bc2a47b0d519759194869bcbb
Geert Kapteijns <geertkapteijns@wireless32-160.cwi.nl>
2015-03-06 14:23:24 +0100

    moved extension of rubytree library to util/tree.rb

--- lib/quartet_tree.rb
+++ lib/quartet_tree.rb
@@ -1,4 +1,5 @@
 require 'tree'
+require_relative "util/tree"
 require 'byebug'
 
 module Clustering
@@ -10,6 +11,7 @@ module Clustering
     # @param distance_matrix [DistanceMatrix]
     def initialize(distance_matrix)
       raise Exception "Can't cluster fewer than 4 items." if distance_matrix.size < 4
+
       @distance_matrix = distance_matrix
       @tree = create_random_tree
     end
@@ -148,20 +150,3 @@ module Clustering
   end
 end
 
-class Tree::TreeNode
-  def swap_leaves(a, b)
-    old_a = a.detached_copy; old_b = b.detached_copy
-    parent_a = a.parent; parent_b = b.parent
-
-    # If parents are the same, TreeNode#add throws an error. So we do not swap at all.
-    return a if parent_a == parent_b
-
-    # After this, a is parentless and b has a's parent.
-    a.replace_with(b) 
-
-    parent_b.add(old_a, 0)
-  end
-
-  def swap_subtrees(a, b)
-  end
-end
