0ac4b492f4a0f103cbe73b907b6d58365c0dfbaa
Geert Kapteijns <geertkapteijns@wireless32-160.cwi.nl>
2015-03-06 14:01:14 +0100

    removed old, unnecessary files

--- initialize.rb
+++ initialize.rb
@@ -1,14 +1,1 @@
-# For easy loading in IRB.
-
-require_relative "quartet_method"
-require_relative "test_quartet_tree"
-
-@tree = Clustering::QuartetTree.from_directory("10-mammals")
-@optimized_tree = @tree.maximize_benefit_score
-
-#@gauge_tree = TestQuartetTree.random(5)
-#@test_tree  = TestQuartetTree.random(5)
-
-#@test_tree.distance_matrix = @gauge_tree.distance_matrix
-
 
