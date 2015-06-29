f0430f3425c104b4f7c895a6189ae265633b8141
Kappie <ghkapteijns@gmail.com>
2015-06-29 13:13:10 +0200

    hoi

--- lib/clustering.R
+++ lib/clustering.R
@@ -1,6 +1,6 @@
 library(ape)
 
-distances <- "distance_matrices/30-source-files-no-comments.csv" #paste("distance_matrices/", dir_name, sep="") 
+distances <- "distance_matrices/random-files.csv" #paste("distance_matrices/", dir_name, sep="") 
 
 #distance_matrix <- read.table(distances, row.names=1)
 
@@ -8,7 +8,7 @@ distance_matrix <- as.dist( as.matrix( read.csv(distances, row.names=1 ) ) )
 clustering <- hclust(distance_matrix, method="average" )
 dendrogram <- as.dendrogram(clustering)
 
-pdf("plots/30-source-files-no-comments-average.pdf")
+pdf("plots/random-files-average.pdf")
 #plot( cut(dendrogram, h = 0.90)[[1]])
 plot( clustering, ann=FALSE, ylab=FALSE)
 dev.off()
