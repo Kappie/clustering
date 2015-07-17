0416106c0500f23fecff8d5b76974c91901de30b
Kappie <ghkapteijns@gmail.com>
2015-07-17 14:51:20 +0200

    blaf

--- lib/clustering.R
+++ lib/clustering.R
@@ -1,14 +1,11 @@
 library(ape)
 
-distances <- "distance_matrices/random-files.csv" #paste("distance_matrices/", dir_name, sep="") 
-
-#distance_matrix <- read.table(distances, row.names=1)
+distances <- "distance_matrices/latin_texts.csv" 
 
 distance_matrix <- as.dist( as.matrix( read.csv(distances, row.names=1 ) ) )
-clustering <- hclust(distance_matrix, method="average" )
+clustering <- hclust(distance_matrix, method="complete" )
 dendrogram <- as.dendrogram(clustering)
 
-pdf("plots/random-files-average.pdf")
-#plot( cut(dendrogram, h = 0.90)[[1]])
+pdf("plots/latin-texts-complete")
 plot( clustering, ann=FALSE, ylab=FALSE)
 dev.off()
